ec3745bb060a94f1e5b9552b067c4fce5c32cd84
swj <swj@donga.com>
2023-01-24 16:43:19 +0900

    .

---
+++ src/main/java/com/example/spring_prac/app/v1/OrderServiceV1.java
@@ -0,0 +1,14 @@
+package com.example.spring_prac.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service @RequiredArgsConstructor
+public class OrderServiceV1 {
+    private final OrderRepositoryV1 orderRepositoryV1;
+
+    public void orderItem(String itemId) {
+        orderRepositoryV1.save(itemId);
+
+    }
+}
